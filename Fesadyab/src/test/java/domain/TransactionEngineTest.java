37844010c819c5a4a476bf26927dca4950f20c2a
kasrahj <hajiheidari.k@gmail.com>
2024-11-15 20:29:07 +0330

    Test: add tests for TransactionEngine

--- Fesadyab/src/test/java/domain/TransactionEngineTest.java
+++ Fesadyab/src/test/java/domain/TransactionEngineTest.java
@@ -1,4 +1,127 @@
 package domain;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
 public class TransactionEngineTest {
+//    private Transaction transaction;
+//    private TransactionEngine transactionEngine;
+//
+//    @BeforeEach
+//    void setUp() {
+//        transaction = new Transaction();
+//        transactionEngine = new TransactionEngine();
+//    }
+//
+//    @AfterEach
+//    void tearDown() {
+//        transaction = null;
+//        transactionEngine = null;
+//    }
+    private static TransactionEngine CreateTransactionEngine(ArrayList<Transaction> transactionHistory)
+    {
+        TransactionEngine transactionEngine = new TransactionEngine();
+        transactionEngine.transactionHistory = transactionHistory;
+        return transactionEngine;
+    }
+
+    private static Transaction CreateTransactions(int transactionId, int accountId , int amount, boolean isDebit)
+    {
+        Transaction transaction = new Transaction();
+        transaction.setAccountId(accountId);
+        transaction.setAmount(amount);
+        transaction.setTransactionId(transactionId);
+        transaction.setDebit(isDebit);
+        return transaction;
+    }
+
+    static Object[][] transactionAverageAmountParameters() {
+        ArrayList<Transaction> transactionHistory = new ArrayList<>(List.of(
+                CreateTransactions(1,1, 10, true),
+                CreateTransactions(2,2, 26, true),
+                CreateTransactions(3,1, 30, true)
+        ));
+
+        TransactionEngine transactionEngine = CreateTransactionEngine(transactionHistory);
+
+        return new Object[][] {
+                { transactionEngine, 1, 20 },
+                { transactionEngine, 2, 26 },
+                { transactionEngine, 3, 0 }
+        };
+    }
+    @ParameterizedTest
+    @MethodSource("transactionAverageAmountParameters")
+    void shouldReturnCorrectAverageTransactionAmountForAccount(TransactionEngine transactionEngine,
+                                                               int accountId, int expectedAverageTransactionAmount) {
+        assertEquals(expectedAverageTransactionAmount, transactionEngine.getAverageTransactionAmountByAccount(accountId));
+    }
+
+    static Object[][] transactionPatternAboveThresholdParameters() {
+        ArrayList<Transaction> transactionHistory = new ArrayList<>(List.of(
+                CreateTransactions(1,1, 5, true),
+                CreateTransactions(2,2, 13, true),
+                CreateTransactions(3,3, 19, true),
+                CreateTransactions(4,4,33, true)
+        ));
+
+        TransactionEngine transactionEngine = CreateTransactionEngine(transactionHistory);
+        TransactionEngine emptyTransactionEngine = CreateTransactionEngine(new ArrayList<>());
+
+
+        return new Object[][] {
+                { emptyTransactionEngine , 6, 0 },
+                { transactionEngine, 50, 0 },
+                { transactionEngine, 14, 14 },
+                { transactionEngine, 10, 0 },
+        };
+    }
+    @ParameterizedTest
+    @MethodSource("transactionPatternAboveThresholdParameters")
+    void shouldReturnCorrectTransactionPatternAboveThreshold(
+            TransactionEngine transactionEngine, int threshold, int expectedTransactionPatternAboveThreshold) {
+
+        assertEquals(expectedTransactionPatternAboveThreshold,
+                transactionEngine.getTransactionPatternAboveThreshold(threshold));
+    }
+
+    static Object[][] transactionFraudScoreParameters() {
+        ArrayList<Transaction> transactionHistory = new ArrayList<>(List.of(
+                CreateTransactions(1,1, 10, true),
+                CreateTransactions(2,1, 5, true),
+                CreateTransactions(3,1, 15, true),
+                CreateTransactions(4,2, 20, true)
+        ));
+
+        TransactionEngine transactionEngine = CreateTransactionEngine(transactionHistory);
+
+        return new Object[][] {
+                { transactionEngine, CreateTransactions(4,1, 12, false), 0 },
+                { transactionEngine, CreateTransactions(4,1, 8, true), 0 },
+                { transactionEngine, CreateTransactions(4,1, 30, true), 10 },
+        };
+    }
+    @ParameterizedTest
+    @MethodSource("transactionFraudScoreParameters")
+    void shouldReturnCorrectFraudNumber(TransactionEngine transactionEngine, Transaction transaction,
+                                                     int expectedFraud) {
+        assertEquals(expectedFraud, transactionEngine.detectFraudulentTransaction(transaction));
+    }
+
+
+
+
+
+
+
 }
--- Fesadyab/src/test/java/domain/TransactionTest.java
+++ Fesadyab/src/test/java/domain/TransactionTest.java
@@ -1,4 +1,51 @@
 package domain;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class TransactionTest {
+    private Transaction transaction;
+    private TransactionEngine transactionEngine;
+
+
+    @BeforeEach
+    void setUp() {
+        transaction = new Transaction();
+        transactionEngine = new TransactionEngine();
+    }
+
+    @AfterEach
+    void tearDown() {
+        transaction = null;
+        transactionEngine = null;
+    }
+
+    @Test
+    void EqualsShouldReturnTrueWhenTransactionIsEqual()
+    {
+        assertTrue(transaction.equals(transaction));
+        Transaction transaction1 = new Transaction();
+        transaction.setTransactionId(1);
+        transaction1.setTransactionId(1);
+        assertTrue(transaction.equals(transaction1));
+    }
+
+    @Test
+    void EqualsShouldReturnFalseWhenTransactionIsNotEqual()
+    {
+        Transaction transaction1 = new Transaction();
+        transaction.setTransactionId(1);
+        transaction1.setTransactionId(2);
+        assertFalse(transaction.equals(transaction1));
+    }
+    @Test
+    void EqualsShouldReturnFalseWhenObjectIsNotInstanceOfTransactionClass()
+    {
+        assertFalse(transaction.equals(transactionEngine));
+    }
+
 }
